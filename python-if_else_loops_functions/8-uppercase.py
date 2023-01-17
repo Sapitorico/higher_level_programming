5c351128efe755742ef9bdd47319d736b21e2eab
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 20:01:14 -0300

    prints a string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            print(chr(ord(i) - 32), end="")
+        else:
+            print(i, end="")
+        print()
