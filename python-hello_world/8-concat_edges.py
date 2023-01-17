598e8eb478fb69db7c76fa5e810379e8923b0a4e
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 11:09:37 -0300

    fix  E501 line too long (99 > 79 characters)

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split()[5] + str[6] + str.split()[6] + str[6] + str.split()[12] + str[6] + str.split()[0]
+str = str.split()[5]+str[6]+str.split()[6]+str[6]+str.split()[12]+str[6]+str.split()[0]
 print(str)
