c65332290505fa77607dd26a284f834b6e204669
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 13:52:58 -0300

    change type

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
+last_digit = int(repr(number)[-1])
 if number < 0:
     last_digit = last_digit * -1
 print("Last digit of", number, "is", last_digit, end="")
