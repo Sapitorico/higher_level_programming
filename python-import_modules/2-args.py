154a63627986e11fcde8fee2b7be0ddddd967ad0
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 21:12:01 -0300

    prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+    elif n == 1:
+        print("{:d} argument:".format(n))
+    else:
+        print("{:d} arguments:".format(n))
+        for i in range(1, n + 1):
+            print("{:d}: {:s}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    print_arg(sys.argv)
