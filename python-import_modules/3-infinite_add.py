5092dd9708a1b7509e0852463264b528cf202be9
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 21:18:32 -0300

    prints the result of the addition of all arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_arg(argv):
+    if len(argv) == 1:
+        print("0")
+        return
+    else:
+        add = 0
+        for i in range(1, len(argv)):
+            add += int(argv[i])
+        print("{:d}".format(add))
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
