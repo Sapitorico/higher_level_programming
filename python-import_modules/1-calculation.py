b789c9d0061a040539f22e5bb836318bb9e0a3b1
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 21:07:28 -0300

    fix style

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    add_result = add(a, b)
-    sub_result = sub(a, b)
-    mul_result = mul(a, b)
-    div_result = div(a, b)
-    print('{}'.format(add_result))
-    print('{}'.format(sub_result))
-    print('{}'.format(mul_result))
-    print('{}'.format(div_result))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
