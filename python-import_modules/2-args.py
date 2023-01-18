becdf64ce1a3acae197e6f174c412b0cc7764a92
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-18 09:33:36 -0300

    fix style code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,15 @@
 def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
-        print("{:d} argument.".format(n))
+        print("{:d} arguments.".format(n))
     elif n == 1:
-        print("{:d} argument:".format(n))
+        print("{:d} arguments:".format(n))
     else:
         print("{:d} arguments:".format(n))
     for i in range(1, n + 1):
-        print("{:d}: {:s} ".format(i, argv[i]))
+        print("{:d}: {:s}".format(i, argv[i]))
+
+
 if __name__ == "__main__":
     import sys
     print_arg(sys.argv)
