4cd6f73f98c01b0704006ac50452dd35dfcb1e09
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-18 09:30:05 -0300

    fix code style

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_arg(argv):
-    n = len(argv) -1
+    n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
     elif n == 1:
@@ -9,6 +9,7 @@ def print_arg(argv):
         print("{:d} arguments:".format(n))
     for i in range(1, n + 1):
         print("{:d}: {:s}".format(i, argv[i]))
+
 if __name__ == "__main__":
     import sys
     print_arg(sys.argv)
