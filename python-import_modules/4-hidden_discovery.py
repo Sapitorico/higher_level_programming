e39cbc8c1b8a45fa97577e7beffbed3bbdcf07fc
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 21:24:59 -0300

    Who are you?

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if not i.startswith('__'):
+            print("{:s}".format(i))
+if __name__ == "__main__":
+    discovr()
