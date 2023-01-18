40c72950dcb1bb4798ebef913ad9d08548e060a8
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 21:01:30 -0300

    add format

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,7 +2,11 @@
 from calculator_1 import add, subtract, multiply, divide
 a = 10
 b = 5
-print(add(a, b))
-print(subt(a, b))
-print(mul(a, b))
-print(div(a, b))
+add_result = add(a, b)
+sub_result = sub(a, b)
+mul_result = mult(a, b)
+div_result = div(a, b)
+print('{}'.format(add_result))
+print('{}'.format(sub_result))
+print('{}'.format(mul_result))
+print('{}'.format(div_result))
