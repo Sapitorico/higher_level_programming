e95b673492ce52c3c67b7b1436d12d291bb2137d
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-17 21:03:14 -0300

    if __name__ == "__main__":

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
-a = 10
-b = 5
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mult(a, b)
-div_result = div(a, b)
-print('{}'.format(add_result))
-print('{}'.format(sub_result))
-print('{}'.format(mul_result))
-print('{}'.format(div_result))
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiply, divide
+    a = 10
+    b = 5
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mult(a, b)
+    div_result = div(a, b)
+    print('{}'.format(add_result))
+    print('{}'.format(sub_result))
+    print('{}'.format(mul_result))
+    print('{}'.format(div_result))
