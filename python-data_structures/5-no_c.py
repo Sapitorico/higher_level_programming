ac588586553a4ff31bde746246f18230a006c7f1
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 12:30:54 -0300

    removes all characters c and C from a string.

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if not my_string:
-        return
-    return my_string.translate({ord(letter): None for letter in 'c' and 'C'})
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
