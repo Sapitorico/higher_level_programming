d4ac9c52e2da6f3a8d5dc71a13c81c1ea2815b94
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 20:08:01 -0300

    qwsqws

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,5 +2,8 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    result = tuple(map(sum,zip(tuple_a, tuple_b)))
-    return result
+    t1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    t2 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    t3 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    t4 = tuple_b[1] if len(tuple_b) >= 2 else 0
+    return (t1 + t3, t2 + t4)
