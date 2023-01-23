c86d26c674252f6d5ba3313f4800281be44296c4
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 13:07:32 -0300

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return
     for index1 in range(len(matrix)):
-        for index2 in range(len(matrix[index1]) -1):
-            print("{:d} ".format(matrix[index1][index2]), end="")
+        for index2 in range(len(matrix[index1])):
+            print("{:d}".format(matrix[index1][index2]), end=" ")
         print()
