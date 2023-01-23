7f3f6f781990f4a5b1f1677e5941ee6868e7f193
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 09:36:51 -0300

    fix outputs

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx <= 0:
         return None
-    if len(my_list) < idx:
+    if len(my_list) <= idx:
         return None
     return my_list[idx]
--- python-data_structures/1-main.py
+++ python-data_structures/1-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 element_at = __import__('1-element_at').element_at
 
-list = [1, 2, 3]
-idx = 0
-print("Element at index {:d} is {:d}".format(idx, element_at(list, idx)))
+list = [1]
+idx = 1
+print("Element at index {:d} is {}".format(idx, element_at(list, idx)))
