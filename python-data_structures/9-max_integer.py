7a5f82c4323d98eb8269331d449d24727957394c
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 20:09:40 -0300

    missing whitespace after ','

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,7 +6,7 @@ def max_integer(my_list=[]):
         return None
     else:
         max_int = my_list[0]
-        for i in range(1,len(my_list)):
+        for i in range(1, len(my_list)):
             if my_list[i] > max_int:
                 max_int = my_list[i]
         return max_int
