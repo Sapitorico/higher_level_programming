b998a78d1593ce4a0e51f1549450d19e21d021bd
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 19:39:32 -0300

    qwdqwdqwd

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,7 @@
 
 def multiple_returns(sentence):
     if not sentence:
-        sentence[0] = None
+        tuple_1 = (0, None)
+        return tuple_1
     tuple_1 = (len(sentence), sentence[0])
     return tuple_1
