46b69d08cd17727e42cb6066877026e09322a21c
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 10:31:53 -0300

    fix pepocho

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-
 def new_in_list(my_list, idx, element):
-	if idx < 0 or idx >= len(my_list):
-		return my_list
-	new_list = list.copy(my_list)
-	new_list[idx] = element
-	return new_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = list.copy(my_list)
+    new_list[idx] = element
+    return new_list
