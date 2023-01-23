2fd5210481441c5f13cce692b6c126ad3f9a9295
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 10:01:09 -0300

    if ids is negative or is out range, not modify, and return a original list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,9 +2,9 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     if idx >= len(my_list):
-        return None
+        return my_list
     my_list[idx] = element
     return my_list
 
