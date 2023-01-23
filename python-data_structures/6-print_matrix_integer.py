518b9a5905a6af15ac54cb00eba09836f012b644
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-23 13:01:25 -0300

    prints a matrix of integers.

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+    for index1 in range(len(matrix)):
+        for index2 in range(len(matrix[index1])):
+            print("{:d} ".format(matrix[index1][index2]), end="")
+        print()
