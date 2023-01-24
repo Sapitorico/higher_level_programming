773c939f4d05012cbdbac31b80ba532b14720eec
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-24 14:42:52 -0300

    fix

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def simple_delete(a_dictionary,key):
-    if isinstance(a_dictionary,dict) and isinstance(key,str):
+def simple_delete(a_dictionary, key):
+    if isinstance(a_dictionary, dict) and isinstance(key, str):
         del (a_dictionary[key])
 
     return None
