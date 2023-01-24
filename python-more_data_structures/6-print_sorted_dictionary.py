122e6a5c77e7e37011db68f0ae305210de918933
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-24 14:23:29 -0300

    add a space

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
     keys = a_dictionary.keys()
     sortedKeys = sorted(keys)
     for key in sortedKeys:
-        print("{}:{}".format(key, a_dictionary[key]))
+        print("{}: {}".format(key, a_dictionary[key]))
