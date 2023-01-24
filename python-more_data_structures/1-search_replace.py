60428ab64255173339e64df92e486f0a5cf3e311
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-24 11:54:20 -0300

    fix case if not exit list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@
 
 def search_replace(my_list, search, replace):
     if not my_list:
-        return
+        return my_list
     new_list = [x if x != search else replace for x in my_list]
     return new_list
