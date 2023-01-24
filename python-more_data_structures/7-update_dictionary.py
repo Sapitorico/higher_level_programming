af02572c491360a51fb12a404a4a092c0d474bb5
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-24 14:42:29 -0300

    function that deletes a key in a dictionary.

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def update_dictionary(a_dictionary,key,value):
+def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
-        a_dictionary[key] = value
+        a_dictionary[key] = [value]
