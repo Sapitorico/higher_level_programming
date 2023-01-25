5be82e89aa1bac47ad0e4b51099e40e53cf82944
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-25 06:35:56 -0300

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,11 +4,11 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
-    numeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
     for i in range(len(roman_string)):
-        if i > 0 and numeral[roman_string[i]] > numeral[roman_string[i - 1]]:
-            result += numeral[roman_string[i]] - 2 * numeral[roman_string[i - 1]]
+        if i > 0 and num[roman_string[i]] > num[roman_string[i - 1]]:
+            result += num[roman_string[i]] - 2 * num[roman_string[i - 1]]
         else:
-            result += numeral[roman_string[i]]
+            result += num[roman_string[i]]
     return result
