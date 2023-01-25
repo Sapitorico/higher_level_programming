0becd7fa72031360e2925400f5e50459d7725beb
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-25 06:24:43 -0300

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or not roman_string:
         return 0
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
