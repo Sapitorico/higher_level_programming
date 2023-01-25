ab9dc7d9af567b2b287849ce8131b11dbc24d318
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-25 10:38:31 -0300

    print to elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for index in range(x):
+            print (("{:d}".format(my_list[index])), end="")
+    except:
+        print()
+        return x
+    print()
+    return x
---
+++ python-exceptions/README.md
@@ -0,0 +1,1 @@
+<div><h1>Python - Exceptions</h1></div>
