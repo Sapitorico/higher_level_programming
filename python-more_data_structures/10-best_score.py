28263cb5560d32e47432f38d5e91b16a823c3234
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-25 06:23:33 -0300

    ascwdqw

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,13 +2,4 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary:
-        max_value = 0
-        max_key = None
-        for key, value in a_dictionary.items():
-            if value > max_value:
-                max_value = value
-                max_key = key
-        return max_key
-    else:
-        return None
+    return max(a_dictionary, key=lambda x: a_dictionary[x]) if a_dictionary else None
