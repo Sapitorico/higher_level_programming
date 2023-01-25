bea6f3265f2da26d687149cc8a100da9a4e4558c
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-25 10:45:32 -0300

    fix output

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print (("{:d}".format(my_list[index])), end="")
     except:
         print()
-        return x
+        return index
     print()
     return x
