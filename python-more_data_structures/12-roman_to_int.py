77e619202dfecf7d9276406f0f4918e60aecb803
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-25 06:31:22 -0300

    fwedw

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,13 @@
 
 
 def roman_to_int(roman_string):
-    if type(roman_string) != str or not roman_string:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
-    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_val = 0
+    numeral_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
     for i in range(len(roman_string)):
-        if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i - 1]]:
-            int_val += rom_val[roman_string[i]] - 2 * rom_val[roman_string[i - 1]]
+        if i > 0 and numeral_map[roman_string[i]] > numeral_map[roman_string[i - 1]]:
+            result += numeral_map[roman_string[i]] - 2 * numeral_map[roman_string[i - 1]]
         else:
-            int_val += rom_val[roman_string[i]]
-    return int_val
+            result += numeral_map[roman_string[i]]
+    return result
