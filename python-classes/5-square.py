6dc8924a6d557d34644c1b35c8fd7ada97df1176
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-31 08:43:33 -0300

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-#Printing a square
-''' obtain area od square '''
+''' create a class square '''
 
 
 class Square:
     ''' define the initiation'''
     def __init__(self, size=0):
         self.__size = size
-        '''create a private attribute '''
+
+    '''create a private attribute '''
     @property
     def size(self):
         return self.__size
 
     @size.setter
     def size(self, value):
+
         '''write a size validation'''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -23,5 +24,17 @@ class Square:
             self.__size = value
 
     def area(self):
+
         ''' public method thats return area of square'''
         return self.__size**2
+
+    ''' print a square of # the size of self.__size'''
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for line in range(self.__size):
+                print()
+                for j in range(self.__size):
+                    print('#', end="")
+            print()
