3bca2db983c7fb77c57e2cf443fd885b717254e3
Sapitorico <renzoyaques@gmail.com>
2023-02-03 18:57:01 -0300

    block comment should start with '# '

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-#Printing a square
-''' obtain area od square '''
+# define a square class
 
 
 class Square:
-    ''' define the initiation'''
+    """ create a private instant attribute """
     def __init__(self, size=0):
         self.__size = size
-        '''create a private attribute '''
+
+    """ define a Public instance method """
     @property
     def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        '''write a size validation'''
-        if not isinstance(value, int):
+        """ size must be an integer size must be >= 0 """
+        if type(self.__size) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif self.__size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
 
+    """ define a Public instance method """
+    @property.setter
+    def size(self, value):
+        if type(self.__size) is not int:
+            raise ValueError("size must be an inter")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """ define a Public instance method """
+    @property
     def area(self):
-        ''' public method thats return area of square'''
-        return self.__size**2
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            print("#" * self.__size)
