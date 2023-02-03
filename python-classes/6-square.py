d7e4a937382623bed12eddcffc06508dfa0126f5
Sapitorico <renzoyaques@gmail.com>
2023-02-03 19:43:47 -0300

    print square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,9 +4,8 @@
 
 class Square:
     ''' define the initiation'''
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0):
         self.__size = size
-        self.__position = position
 
     '''create a private attribute '''
     @property
@@ -15,6 +14,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+
         '''write a size validation'''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -23,18 +23,8 @@ class Square:
         else:
             self.__size = value
 
-    @property
-    def position(self):
-        ''' public method thats return position of square'''
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple, int) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
     def area(self):
+
         ''' public method thats return area of square'''
         return self.__size**2
 
@@ -43,13 +33,9 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for line in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for space in range(self.__position[0]):
-                    print(" ", end="")
+            for line in range(self.__size):
                 for j in range(self.__size):
                     print('#', end="")
-                if i+1 < self.__size:
+                if line+1 < self.__size:
                     print()
             print()
