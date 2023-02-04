ac128a8c223ff9adeefb8106fb8c593a847d2a5e
Sapitorico <renzoyaques@gmail.com>
2023-02-04 06:50:44 -0300

    fix typerror menssege in position seter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -30,10 +30,11 @@ class Square:
     """ define a Public instance method """
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2 or\
-            type(value[0]) is not int or type(value[1]) is not int or\
+        if type(value) is not tuple or len(value) != 2 or \
+            type(value[0]) is not int or type(value[1]) is not int or \
                 value[0] < 0 or value[1] < 0:
-                    raise TypeError("position must be a tuple of 2 positive integers")
+                    raise TypeError("position must be a \
+tuple of 2 positive integers")
         self.__position = value
 
     """ define a Public instance method """
