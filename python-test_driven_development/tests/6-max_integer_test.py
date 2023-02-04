5bab2a3f765a11d0f4ea92fa6e2566d1caf37258
Sapitorico <renzoyaques@gmail.com>
2023-02-04 20:42:23 -0300

    add a test max in the middle and beginning

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,7 +8,8 @@ class TestMaxInteger(unittest.TestCase):
     """ unittest for the function def max_integer(list=[]):. """
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
-        self.assertEqual(max_integer([1, 2, 3, 4, 5, 6]), 6)
+        self.assertEqual(max_integer([6, 2, 3, 4, 5, 1]), 6)
+        self.assertEqual(max_integer([1, 2, 3, 6, 5, 4]), 6)
 
     def test_max_integer_empty(self):
         """ check a list si not empty """
