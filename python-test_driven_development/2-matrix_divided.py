7ef85e289d82f6f5da5a9f765f1ab2632b5a5ad7
Sapitorico <renzoyaques@gmail.com>
2023-02-04 14:42:04 -0300

    function that divides all elements of a matrix.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-""" function divides ll elements fo a matrix """
+""" function that divide elements to matrix """
 
 
 def matrix_divided(matrix, div):
-    """ matrix must be a list of lists """
-    if not all([isinstance(row, list) for row in matrix]):
-        raise TypeError("matrix must be a matrix \
-            (list of lists) of integers/floats")
-    # for i in range(len(matrix)):
-    #     for j in range(len(matrix[0])):
-    #         matrix[i][j] = round(matrix[i][j]/div, 2)
-    """ matrix must be a list of lists of integers or floats """
-    if not all([isinstance(j, (int, float)) for row in matrix for j in row]):
-        raise TypeError("matrix must be a \
-matrix (list of lists) of integers/floats")
+    """ matrix must be a list of lists of integers or floats, """
+    if not all(isinstance(matrix, list) for index in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(j, (int, float)) for row in matrix for j in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     """ Each row of the matrix must be of the same size """
-    if not all([len(matrix[0]) == len(row) for row in matrix]):
-        raise TypeError("Each row of the matrix must have the same size")
-    """ div must be a number (integer or float) """
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    """ div can’t be equal to 0 """
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+    if len(matrix[0]) != div:
+        raise ValueError("Each row of the matrix must have the same size")
+    """ return All elements of the matrix should be divided by div, rounded to 2 decimal places """
     return [[round(num/div, 2) for num in row] for row in matrix]
