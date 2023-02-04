89e370de57b19a5a0c3a2fe9fab8117f90fdaef1
Sapitorico <renzoyaques@gmail.com>
2023-02-04 06:44:40 -0300

    positon must not be an attribute private

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -30,12 +30,10 @@ class Square:
     """ define a Public instance method """
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[1]):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not int or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value) is not tuple or len(value) != 2 or\
+            type(value[0]) is not int or type(value[1]) is not int or\
+                value[0] < 0 or value[1] < 0:
+                    raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     """ define a Public instance method """
