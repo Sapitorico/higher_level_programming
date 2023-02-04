3c1e662dc233af7624149e9a5ffeefb258bc96ae
Sapitorico <renzoyaques@gmail.com>
2023-02-04 15:15:41 -0300

    fix pept8

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,9 +5,11 @@
 def matrix_divided(matrix, div):
     """ matrix must be a list of lists of integers or floats, """
     if not all(isinstance(matrix, list) for index in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
     if not all(isinstance(j, (int, float)) for row in matrix for j in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
     """ Each row of the matrix must be of the same size """
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise ValueError("Each row of the matrix must have the same size")
@@ -16,5 +18,6 @@ def matrix_divided(matrix, div):
         raise ValueError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    """ return All elements of the matrix should be divided by div, rounded to 2 decimal places """
+    """ return All elements of the matrix should be \
+        divided by div, rounded to 2 decimal places """
     return [[round(num/div, 2) for num in row] for row in matrix]
