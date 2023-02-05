61b7bb78ee3255d4da872dc2200f91f928e5cf65
Sapitorico <renzoyaques@gmail.com>
2023-02-05 14:08:13 -0300

    calculate are and perimeter

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" create a empty class Rectangle """
+
+
+class Rectangle:
+    """ empty field """
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    """ Private instance attribute: width """
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        """ if width is less than 0 """
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """ Private instance attribute: height """
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height must be an integer """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        """ if height is less than 0 """
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    """ Public instance method """
+    def area(self):
+        return self.__width * self.__height
+
+    """ Public instance method """
+    def perimeter(self):
+        """ if width or height is equal to 0, perimeter is equal to 0 """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
