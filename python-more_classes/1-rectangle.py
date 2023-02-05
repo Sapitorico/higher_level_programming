e6453564398734023f36c7dd7c76f52143418d1b
Sapitorico <renzoyaques@gmail.com>
2023-02-05 13:35:30 -0300

    fix a mensege errros

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,7 +5,15 @@
 class Rectangle:
     """ empty field """
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be integer")
+        if width < 0:
+            raise ValueError(" width must be >= 0")
         self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be integer")
+        if height < 0:
+            raise ValueError(" height must be >= 0")
         self.__height = height
 
     """ Private instance attribute: width """
