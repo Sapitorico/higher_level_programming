5423333900c1cab202c5c6f2512599b0aaa21be2
Sapitorico <renzoyaques@gmail.com>
2023-02-06 19:52:31 -0300

    import a basegeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
-""" Write an empty class BaseGeometry. """
-
-
-class BaseGeometry:
-    """ Public instance method """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    """ Public instance method """
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-""" class Rectangle that inherits from BaseGeometry """
+""" subclass Rectangle that inherits from BaseGeometry """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -23,7 +8,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ width and height must be positive integers,\
             validated by integer_validator """
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
