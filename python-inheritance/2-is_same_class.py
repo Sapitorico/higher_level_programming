2cf608ac267ba8523b4cfe11f1a37d94660b29ed
Sapitorico <renzoyaques@gmail.com>
2023-02-06 14:58:47 -0300

    check if a smae class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" write a function that return true or false\
+    f the object is exactly an instance of the specified class"""
+
+def is_same_class(obj, a_class):
+    return isinstance(obj, a_class)
