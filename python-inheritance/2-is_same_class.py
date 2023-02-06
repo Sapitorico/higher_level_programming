471572b125cd31c84c53af083e3e786f6eceff24
Sapitorico <renzoyaques@gmail.com>
2023-02-06 15:07:02 -0300

    fix a type isinstance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@
 
 def is_same_class(obj, a_class):
     """ return false or true """
-    return isinstance(type(obj), a_class)
+    if isinstance(type(obj), type(a_class)):
+        return True
+    else:
+        return False
