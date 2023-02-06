002ac618936815907173d1cb404d3b91082b0273
Sapitorico <renzoyaques@gmail.com>
2023-02-06 16:29:31 -0300

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,8 @@
 
 def inherits_from(obj, a_class):
     """ check if the object is an instance of a class """
-    if type(obj) is float or obj is None:
+    if obj is None and a_class is object:
+        return True
+    elif type(obj) is float or obj is None:
         return False
     return type(obj) is not a_class
