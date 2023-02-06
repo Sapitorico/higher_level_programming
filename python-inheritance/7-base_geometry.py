a8f3b5764802e38319cbaa68bdc8c440105e4236
Sapitorico <renzoyaques@gmail.com>
2023-02-06 16:43:16 -0300

    add a new method

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Write an empty class BaseGeometry. """
+
+
+class BaseGeometry:
+    """ Public instance method """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """ Public instance method """
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater that 0")
