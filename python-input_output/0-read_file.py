cd73457853906dfa75e9075c63c00b055dcb7e81
Sapitorico <renzoyaques@gmail.com>
2023-02-07 09:02:47 -0300

    remove a new line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(self):
     """ must use the with """
     with open(self, 'r') as f:
-        [print(line) for line in f.readlines()]
+        [print(line, end="") for line in f.readlines()]
