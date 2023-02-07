7c9ef5ed04431e09a6a71aa680cc67fd6eb06498
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-02-07 13:02:20 -0300

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+/usr/bin/python^M
 """ adds all arguments to a Python list, and then save them to a file """
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
