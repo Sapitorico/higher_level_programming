74f116930be9cfbcf98de8215e03fc461bddec8c
Sapitorico <renzoyaques@gmail.com>
2023-02-07 10:14:38 -0300

    return a objetc data struct representation

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  function that returns an object \
+    (Python data structure) represented by a JSON string: """
+import json
+
+
+def from_json_string(my_str):
+    """ object (Python data structure) represented by a JSON """
+    return json.loads(my_str)
