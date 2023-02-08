92d77d2b66a59c4fb3346a2c2c69d6ecf9999dea
Sapitorico <renzoyaques@gmail.com>
2023-02-08 09:15:27 -0300

    only attributes names cointein

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,23 +3,19 @@
 
 
 class Student:
-    """ Public instance attributes: """
     def __init__(self, first_name, last_name, age):
+        """Initializes the Student class """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    """ Public method  """
     def to_json(self, attrs=None):
-        """ only attribute names contained in this list must be retrieved\
-            Otherwise, all attributes must be retrieved """
+        """Retrieves a dictionary representation of the student instance"""
         if attrs is None:
-            attrs = ["first_name", "last_name", "age"]
-        attrs.append(self.age)
-        attrs.append(self.first_name)
-        attrs.append(self.last_name)
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+            return self.__dict__
+        else:
+            student_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    student_dict[attr] = getattr(self, attr)
+            return student_dict
