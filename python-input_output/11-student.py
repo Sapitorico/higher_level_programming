e3ece6742f35b609f70856ce58367d79afe72173
Sapitorico <renzoyaques@gmail.com>
2023-02-08 09:21:00 -0300

    replaces all attributes of the Student ~

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Write a class Student """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            student_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    student_dict[attr] = getattr(self, attr)
+            return student_dict
+
+    """ Public method  """
+    def reload_from_json(self, json):
+        """Reloads the student instance from a dictionary"""
+        for attr in json:
+            if hasattr(self, attr):
+                setattr(self, attr, json[attr])
