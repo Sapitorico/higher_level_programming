0a426425044d2d5cb783d9fe61b556d059642ebe
Sapitorico <renzoyaques@gmail.com>
2023-02-13 12:27:15 -0300

    each with its own public getter and setter~

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,17 +4,44 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """ Private instance attributes """
-    __width = 0
-    __height = 0
-    __x = 0
-    __y = 0
-    """ Class constructor """
+    """ Class constructor: """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Private instance attributes """
-        __width = width
-        __height = height
-        __x = x
-        __y = y
-        """ Call the super class with id """
         super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    """ Private instance attributes,\
+        each with its own public getter and setter: """
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
