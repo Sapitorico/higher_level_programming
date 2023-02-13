50bddca17fa27daec7a8d21516c427a42add7126
Sapitorico <renzoyaques@gmail.com>
2023-02-13 10:49:35 -0300

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,12 +4,16 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    __width = 0
+    __height = 0
+    __x = 0
+    __y = 0
     """ Class constructor """
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Private instance attributes """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        __width = width
+        __height = height
+        __x = x
+        __y = y
         """ Call the super class with id """
         super().__init__(id)
