f29b8606892ecdc1a2c193bb532dec8d19f41f77
Sapitorico <renzoyaques@gmail.com>
2023-02-17 08:30:24 -0300

    12. Square update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,11 +8,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ validation must inherit from Rectangle """
         super().__init__(size, size, x, y, id)
+        self.x = x
+        self.y = y
+        self.id = id
 
     def __str__(self):
         """ The overloading """
-        return "[Square] ({}) {}/{} - {}"\
-            .format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id,\
+            self.x, self.y, self.size)
 
     """ getter """
     @property
@@ -30,11 +33,14 @@ class Square(Rectangle):
     """ method """
     def update(self, *args, **kwargs):
         """ dwdqwd """
-        if len(args) > 0:
-            self.size = args[0]
-        if len(args) > 1:
-            self.x = args[1]
-        if len(args) > 2:
-            self.y = args[2]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
         for key, value in kwargs.items():
             setattr(self, key, value)
