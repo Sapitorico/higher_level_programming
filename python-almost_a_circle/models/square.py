2a1e7918785a59b41de51a70d17f8f676cf11561
Sapitorico <renzoyaques@gmail.com>
2023-02-17 08:34:29 -0300

    read a next line

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,8 +14,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """ The overloading """
-        return "[Square] ({}) {}/{} - {}"
-    .format(self.id, self.x, self.y, self.size)
+        return "[Square] ({}) {}/{} - {}"\
+.format(self.id, self.x, self.y, self.size)
 
     """ getter """
     @property
