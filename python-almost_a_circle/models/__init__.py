d3706294748887a454d8a4f08fab2ed4b0835e2f
Sapitorico <renzoyaques@gmail.com>
2023-02-17 20:31:28 -0300

    test

---
+++ python-almost_a_circle/models/__init__.py
@@ -0,0 +1,4 @@
+""" the file will become a Python package """
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
---
+++ python-almost_a_circle/tests/base_tests.py
@@ -0,0 +1,66 @@
+`""" write a test from class base"""
+import unittest
+from models import Base
+
+
+class TestBase(unittest.TestCase):
+    """ Test class """
+    def test_id(self):
+        """ test id """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
+
+    def test_to_json_string(self):
+        """ test to json string """
+        r1 = Base.to_json_string(None)
+        self.assertEqual(r1, "[]")
+        r2 = Base.to_json_string([])
+        self.assertEqual(r2, "[]")
+        r3 = Base.to_json_string([{'id': 89}])
+        self.assertEqual(r3, '[{"id": 89}]')
+
+    def test_from_json_string(self):
+        """ test from json string """
+        r1 = Base.from_json_string(None)
+        self.assertEqual(r1, [])
+        r2 = Base.from_json_string("[]")
+        self.assertEqual(r2, [])
+        r3 = Base.from_json_string('[{"id": 89}]')
+        self.assertEqual(r3, [{'id': 89}])
+
+    def test_create(self):
+        """ test create """
+        r1 = Base.create(None)
+        self.assertEqual(r1, None)
+        r2 = Base.create([])
+        self.assertEqual(r2, [])
+        r3 = Base.create([{'id': 89}])
+        self.assertEqual(r3, [{'id': 89}])
+
+    def test_save_to_file(self):
+        """ test save to file """
+        r1 = Base.save_to_file(None)
+        self.assertEqual(r1, None)
+        r2 = Base.save_to_file([])
+        self.assertEqual(r2, [])
+        r3 = Base.save_to_file([{'id': 89}])
+        self.assertEqual(r3, [{'id': 89}])
+
+    def test_load_from_file(self):
+        """ test load from file """
+        r1 = Base.load_from_file(None)
+        self.assertEqual(r1, None)
+        r2 = Base.load_from_file([])
+        self.assertEqual(r2, [])
+        r3 = Base.load_from_file([{'id': 89}])
+        self.assertEqual(r3, [{'id': 89}])
+
+
+if __name__ == '__main__':
+    unittest.main()`
