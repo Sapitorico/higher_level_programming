12f25c0ed7d65600718165f65ea38cc0e6c385e8
Sapitorico <renzoyaques@gmail.com>
2023-02-19 17:35:23 -0300

    add a test

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -15,6 +15,13 @@ class TestRectangle(unittest.TestCase):
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
 
+    """ Test of Rectangle(0, 2) exists """
+    def test_rectangle_0_2(self):
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            r2 = Rectangle(2, 0)
+
     def test_width(self):
         """ test width """
         r1 = Rectangle(10, 2)
@@ -59,12 +66,55 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             r4 = Rectangle(10, 2, 0, -8)
 
-    """ Test of Rectangle(0, 2) exists """
-    def test_rectangle_0_2(self):
-        with self.assertRaises(ValueError):
-            r1 = Rectangle(0, 2)
-        with self.assertRaises(ValueError):
-            r2 = Rectangle(2, 0)
+    def test_area(self):
+        """ test area """
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
+
+    def test_display(self):
+        """ test display """
+        r1 = Rectangle(4, 6)
+        r1.display()
+        r2 = Rectangle(2, 2)
+        r2.display()
+        r3 = Rectangle(2, 3, 2, 2)
+        r3.display()
+
+    def test_str(self):
+        """ test str """
+        r1 = Rectangle(4, 6, 2, 1, 12)
+        self.assertEqual(r1.__str__(), "[Rectangle] (12) 2/1 - 4/6")
+
+    def test_update(self):
+        """ test update """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 10/10")
+        r1.update(89, 2)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 2/10")
+        r1.update(89, 2, 3)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 2/3")
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/10 - 2/3")
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
+        r1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 1/3 - 4/2")
+        r1.update(width=1, x=2, y=3, height=4)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 2/3 - 1/4")
+
+    def test_to_dictionary(self):
+        """ test to dictionary """
+        r1 = Rectangle(10, 2, 1, 9)
+        r1_dictionary = r1.to_dictionary()
+        self.assertEqual(r1_dictionary, {'y': 9, 'x': 1, 'id': 12, 'height': 2, 'width': 10})
+        self.assertEqual(type(r1_dictionary), dict)
+
+
 
 
 if __name__ == '__main__':
