351539c5860af11c370a2d7769bd2a1f66bfd1a8
Sapitorico <renzoyaques@gmail.com>
2023-03-13 17:34:39 -0300

    create a second table in databse and add a multiples rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- create a secon table in the database and add a multimples rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCH(256),
+    score INT
+)
+INSERT INTO second_table (id, name, score) VALUES(
+    (1, “John”, 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8)
+)
