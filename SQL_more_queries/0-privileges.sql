e86220a6afcb5cce879822780844ee25d821b30c
Sapitorico <renzoyaques@gmail.com>
2023-03-14 10:46:12 -0300

    test

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- list all privileges for of the users in server
-SELECT user, host, Grant_priv, Super_priv FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
--- SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
