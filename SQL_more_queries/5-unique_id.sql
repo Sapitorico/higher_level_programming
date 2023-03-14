d4bee739fbff8dcd715c17d3c4a2dc438b9c60a8
Sapitorico <renzoyaques@gmail.com>
2023-03-14 18:20:10 -0300

    unique id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,7 @@
 -- create table
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT(1) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
+    UNIQUE (name)
 );
