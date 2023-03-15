a9e67b0513811d6ed7e9edd2e784f1306ad99bee
Sapitorico <renzoyaques@gmail.com>
2023-03-15 14:14:34 -0300

    import a databse

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- import a databse and list all show conteinded in the have at leaest  genere linked
-mysql -uroot -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql;
+mysql -u root -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql;
