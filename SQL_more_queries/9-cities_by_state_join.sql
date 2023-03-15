aaae08423415a5207973388a763a81e839864e5a
Sapitorico <renzoyaques@gmail.com>
2023-03-15 12:21:16 -0300

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- list all cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id;
