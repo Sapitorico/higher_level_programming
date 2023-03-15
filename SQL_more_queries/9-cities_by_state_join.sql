a20ac037e2ee9a710814718d7a076ec58c4da222
Sapitorico <renzoyaques@gmail.com>
2023-03-15 13:01:26 -0300

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.state_id = states.id AND states.name
+WHERE cities.id
 ORDER BY cities.id;
