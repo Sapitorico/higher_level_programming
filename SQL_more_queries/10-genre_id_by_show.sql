8b4f4a143d16888522065f58921e77829d2b9f93
Sapitorico <renzoyaques@gmail.com>
2023-03-16 10:28:33 -0300

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,3 @@
 -- import a databse and list all show conteinded in the have at leaest  genere linked
-mysql -u root -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql;
-SELECT DISTINCT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id;
+mysql -uroot -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql;
+SELECT * FROM tv_genres;
