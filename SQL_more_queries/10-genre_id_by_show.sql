cf6f9890f568a5a2656cb4e6b6fc2620a0780ab5
Sapitorico <renzoyaques@gmail.com>
2023-03-17 16:37:18 -0300

    fix

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- import a databse and list all show conteinded in the have at leaest  genere linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
