54a3ea1d0998fd522cf225324e3c97f89e3abb64
Sapitorico <renzoyaques@gmail.com>
2023-03-18 18:39:03 -0300

    sdas

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 -- SELECT tv_shows.title, tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = show_id ORDER BY title, genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = show_id ORDER BY title, genre_id;
