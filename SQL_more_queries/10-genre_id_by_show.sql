0224480b3a46fff8cdc601ce4bdbd0dec08e54d6
Sapitorico <renzoyaques@gmail.com>
2023-03-18 18:34:05 -0300

    s

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- SELECT tv_shows.title, tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
