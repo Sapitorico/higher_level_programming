c9a7cbbfc048e818b8ab31ff99c8c51880077f16
Sapitorico <renzoyaques@gmail.com>
2023-03-19 23:07:57 -0300

    Cities in state

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the City class
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = 'cities'
+    id = Column(
+        Integer, primary_key=True,
+        nullable=False, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
