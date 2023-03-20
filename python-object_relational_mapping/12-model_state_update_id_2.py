34022ad5c8f72275415d1783be0c0b15aec55080
Sapitorico <renzoyaques@gmail.com>
2023-03-19 22:49:27 -0300

    Update a state

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" script that changes the name of a State
+object from the database hbtn_0e_6_usa """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
