1335949286bc848171a5982362fa76408e34c644
Sapitorico <renzoyaques@gmail.com>
2023-03-19 22:52:51 -0300

    fx

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,15 +11,17 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, db_name))
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, db_name))
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     states = session.query(State).filter(State.name.like('%a%'))
+    print("States with a name containing the letter 'a':")
     for state in states:
-        session.delete(state)
+        print("{}: {}".format(state.id, state.name))
 
-    session.commit()
     session.close()
